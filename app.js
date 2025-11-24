248ec10e655695c1303b09ce86c86382f23b7035
gust-eng <gustavolana232@gmail.com>
2025-11-24 11:47:19 -0300

    Endpoint-SejaBemVindos

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ api.get('/helloworld', (req, resp) => {
     resp.send('Hello World');
 })
 
+api.get('/mensagens/boasvindas', (req, resp) =>{
+    resp.send('Oi, como vai? Sejam bem vindos')
+})
+
+
+api.get('/mensagens/ocupado', (req, resp) =>{
+    resp.send('Estou ocupado no momento')
+})
 
  api.listen(5010, () => console.log("API subiu com sucesso!"));
